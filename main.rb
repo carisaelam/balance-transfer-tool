47af16e2df8d274d5af75bc646ec759d56573e62
carisaelam <carisaelam@gmail.com>
2024-06-30 07:27:36 -0400

    Initial program functions

---
+++ main.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+class Transfer
+  attr_accessor :bucket_balance, :credit_balances
+
+  def initialize(bucket_balance = 0, credit_balances = [])
+    @bucket_balance = bucket_balance
+    @credit_balances = credit_balances
+    @cards = ['Capital One Venture X', 'Capital One Savor', 'Chase Sapphire', 'Southwest Rapid Rewards']
+  end
+
+  def collect_input
+    gets.chomp.to_f
+  end
+
+  def add(array)
+    array.sum.to_f
+  end
+
+  def find_difference(a, b)
+    a - b
+  end
+
+  def calculate_transfer
+    credit_balances = add(@credit_balances)
+    credit_balances - @bucket_balance
+  end
+
+  def collect_bucket_balance
+    print 'Ally bucket balance: $'
+    current_bucket_balance = collect_input.to_f
+    @bucket_balance = current_bucket_balance
+  end
+
+  def collect_credit_balances
+    @cards.each do |card|
+      print "#{card} balance: $"
+      card_balance = collect_input.to_f
+      @credit_balances.push(card_balance)
+    end
+  end
+
+  def print_results
+    total_credit_balance = add(credit_balances)
+    formatted_bucket_balance = format('%.2f', bucket_balance)
+    formatted_transfer_amount = format('%.2f', calculate_transfer)
+
+    puts "\n\nYour total credit card balance is $#{'%.2f' % total_credit_balance}.\n\n"
+    puts "You currently have $#{formatted_bucket_balance} in your Ally bucket.\n\n"
+    puts "You should transfer $#{formatted_transfer_amount} into your Ally bucket\n\n"
+  end
+
+  def start
+    collect_credit_balances
+    collect_bucket_balance
+    calculate_transfer
+    print_results
+  end
+end
+
+test = Transfer.new
+test.start
---
+++ spec/transfer_spec.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+require_relative '../main'
+require 'stringio'
+
+RSpec.describe Transfer do
+  describe '#initialize' do
+    context 'when using default initialization' do
+      subject(:default_balances) { described_class.new }
+
+      it 'bucket is empty' do
+        initialization = default_balances.bucket_balance
+        expect(initialization).to be_zero
+      end
+
+      it 'credit balances are zero' do
+        initialization = default_balances.credit_balances
+        expect(initialization).to eq([])
+      end
+    end
+
+    context 'when specifying both balances' do
+      subject(:current_balances) { described_class.new(500, 300) }
+
+      it 'is 500' do
+        expect(current_balances.bucket_balance).to eq(500)
+      end
+
+      it 'is 300' do
+        expect(current_balances.credit_balances).to eq(300)
+      end
+    end
+  end
+
+  describe '#collect_input' do
+    subject(:user_input) { described_class.new }
+    it 'returns user input' do
+      input = StringIO.new('300')
+      $stdin = input
+      result = user_input.collect_input
+      $stdin = STDIN
+      expect(result).to eq('300')
+    end
+  end
+
+  describe '#add' do
+    subject(:adding) { described_class.new }
+    it 'returns the sum of two numbers' do
+      result = adding.add([1, 2])
+      expect(result).to eq(3)
+    end
+
+    it 'works with more than two numbers' do
+      result = adding.add([1, 2, 3])
+      expect(result).to eq(6)
+    end
+
+    it 'works with positive and negative numbers' do
+      expect(adding.add([1, -2, 3])).to eq(2)
+    end
+
+    it 'returns zero when no input given' do
+      expect(adding.add([])).to be_zero
+    end
+  end
+
+  describe '#find_difference' do
+    subject(:subtracting) { described_class.new }
+    it 'returns the difference of two numbers' do
+      result = subtracting.find_difference(3, 1)
+      expect(result).to eq(2)
+    end
+
+    it 'returns a negative number when second number is higher than first' do
+      result = subtracting.find_difference(1, 3)
+      expect(result).to eq(-2)
+    end
+  end
+
+  describe '#calculate_transfer' do
+    it 'subtracts bucket balance from sum of credit balances' do
+      transfer = described_class.new(300, [100, 200, 300])
+      expect(transfer.calculate_transfer).to eq(300)
+    end
+  end
+end
